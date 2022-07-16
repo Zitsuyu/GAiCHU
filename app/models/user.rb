4807fb8d245a84178bb3b3bc1e73e712aab9fca0
Zitsuyu <yuichi09202001@yahoo.co.jp>
2022-07-16 12:16:11 +0900

    ログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :nickname, presence: true
+
+  has_many :posts
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
